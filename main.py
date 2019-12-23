6e041a8345ef49243cd8f0eedb00c3ece5415140
amoloudi <mohammadaminmoloudi12369@gmail.com>
2019-12-23 14:11:43 +0330

    Update main.py

--- main.py
+++ main.py
@@ -13,11 +13,18 @@ class Task:
 
 
 class Core:
-    def __init__(self):
+    def __init__(self, mu):
         self.idle = True
-        self.rate = 0
+        self.rate = mu                  #pdf server
+        self.speed = -1                 #pdf server
         self.current_task = None
-        self.current_start_time = -1
+        self.current_start_time = -1    #???
+        
+    def set_speed(self):
+        pass
+        
+    def finish_task(self):
+        pass
 
 
 class Server:
@@ -25,10 +32,26 @@ class Server:
         self.cores = cores
         self.queue: list[Task] = []
 
-    def assign_task(self):
+    def assign_task(self):              #set on first free core
         pass
+    
+    def queue_update(self):
+        pass
+    
 
 
 class Scheduler:
-    def __init__(self):
+    def __init__(self, mu):
+        self.rate = mu
+        self.speed = -1
+        self.queue1: list[Task] = []
+        self.queue2: list[Task] = []
+        
+    def set_speed(self):                # mu different for each task!!
         pass
+    
+    def assign_task(self):
+        pass
+    
+def Task_Generator():
+    pass                                #gen arrival(lambda) + deadline(alpha)
